a5ae0721c527b88fb970f2567a876eb35c9e51f6
ozgurege0 <81050186+ozgurege0@users.noreply.github.com>
2022-02-02 16:46:54 +0300

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,7 +7,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="<?php echo $ayarlarcek['ayarlar_url'] ?>">Home</a>
+                <a class="nav-link active" aria-current="page" href="<?php echo $ayarlarcek['ayarlar_url'] ?>">Ana Sayfa</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="<?php echo $ayarlarcek['ayarlar_url'] ?>about.php">Hakkımızda</a>
